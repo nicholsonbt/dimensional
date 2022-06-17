55f73afca822796e8181ecedd3dca76e179f3eff
Ben Nicholson <bennicholson777@gmail.com>
2022-06-17 11:58:57 +0100

    Some functions to compare and calculate distances

---
+++ dimensional.js
@@ -0,0 +1,20 @@
+var DIMENSIONAL = {};
+
+DIMENSIONAL.Dimensions = function(position) {
+	return DIMENSIONAL.GetKeys(position).length;
+}
+
+DIMENSIONAL.GetKeys = function(position) {
+	// Keys will be sorted using insertion sort, as we can expect them to be mostly ordered and small n.
+	return SortedKeys(position);
+}
+
+function SortedKeys(object) {
+	return SORT.InsertionSort(Object.keys(object));
+}
+
+
+DIMENSIONAL.Distance = function(positionA, positionB, metric) {
+	if (!COMPARE.Equals(DIMENSIONAL.GetKeys(positionA), DIMENSIONAL.GetKeys(positionB)))
+		throw "The passed positions have different number of dimensions.";
+}
